c9cf8a20f1d97be82b2e73256250c9cd75f0c516
RenanDrnls <rdorneles64@gmail.com>
2023-01-19 11:42:57 -0300

    Initial commit with first test

---
+++ first-test-project/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int input_number;
+    std::cout << "Hello from my first test project." << std::endl;
+
+    std::cout << "Enter a number between 1 and 100: ";
+    std::cin >> input_number;
+
+    std::cout << "Amazing!! " << input_number << " is my favorite number too!" << std::endl;
+    return 0;
+}
