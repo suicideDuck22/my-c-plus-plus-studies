bcad38453654d0f16856ad62a6543bea9ef312b7
RenanDrnls <rdorneles64@gmail.com>
2023-01-19 13:47:23 -0300

    Added namespace usage for std

--- first-test-project/main.cpp
+++ first-test-project/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 
-int main()
+// Define wich namespace use.
+using namespace std;
+
+/* Can be used by single name, as:
+using std::cout;
+using std::cin;
+using std::endl;
+*/
+
+int main(int argc, char *argv[])
 {
     int input_number;
-    std::cout << "Hello from my first test project." << std::endl;
+    cout << "Hello from my first test project." << endl;
 
-    std::cout << "Enter a number between 1 and 100: ";
-    std::cin >> input_number;
+    cout << "Enter a number between 1 and 100: ";
+    cin >> input_number;
 
-    std::cout << "Amazing!! " << input_number << " is my favorite number too!" << std::endl;
+    cout << "Amazing!! " << input_number << " is my favorite number too!" << endl;
     return 0;
 }
