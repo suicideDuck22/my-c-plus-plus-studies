cc491022cf2bff84efc1a4dda73a46d0649cc27e
suicideDuck22 <rdorneles64@gmail.com>
2023-01-21 11:50:06 -0300

    Some changes, arrays and vectors.

---
+++ arrays-section/arrays.md
@@ -0,0 +1,3 @@
+The name of the array represents the location of the first element, index 0.
+
+No bounds checking.
--- arrays-section/main.cpp
+++ arrays-section/main.cpp
@@ -1,18 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     /*
         Arrays declaration follow this basic pattern => Elements_type Array_name [Array_fixed_size] Optional:{initialize_values_separateds_by_comma}
     */
-    int test_scores [5] {100, 95, 99, 65, 41};
+    int test_scores[5]{100, 95, 99, 65, 41};
 
-    int high_score_per_level [10] {3, 5}; //Initialize the 3 and 5, the rest of the elements are initializeds with 0.
+    int high_score_per_level[10]{3, 5}; // Initialize the 3 and 5, the rest of the elements are initializeds with 0.
 
     const int days_in_year{365};
-    double hi_temperatures [days_in_year] {0}; //Iinitialize all elements to 0.
+    double hi_temperatures[days_in_year]{0}; // Iinitialize all elements to 0.
 
-    int another_array [] {3, 6}; //Make compiler calculate the array size.
-    
+    int another_array[]{3, 6}; // Make compiler calculate the array size.
+
+    cout << "The name of the array is: " << another_array << endl; // Print the address on the memomry.
+
+    int movie_rating[3][4]{
+        {0, 4, 3, 5},
+        {2, 3, 3, 5},
+        {1, 4, 4, 5}}; // Declaring and initializing a matrix.
+
+    double temperatures[]{98.6, 95.2, 88.7, 100.7, 89.0};
+    cout << temperatures[6] << endl;
+
     return 0;
 }
